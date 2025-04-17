3b742628dcc97e1d4134fa32cc0136e3c7ac612a
Joseph <68642130+yusefpasha@users.noreply.github.com>
2025-04-18 00:36:07 +0330

    [FIX] Handle Exact Alarms on Android S and Above
    - Updated `AlarmServiceImpl` to use `setExactAndAllowWhileIdle` when `canScheduleExactAlarms()` returns true on Android S and above.
    - Retained `setAndAllowWhileIdle` for devices below Android S.
    - Removed unnecessary `RECEIVE_BOOT_COMPLETED` permission from `AndroidManifest.xml`.

--- app/src/prefer/java/ir/yusefpasha/taskmanagerapp/data/service/AlarmServiceImpl.kt
+++ app/src/prefer/java/ir/yusefpasha/taskmanagerapp/data/service/AlarmServiceImpl.kt
@@ -5,6 +5,7 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import ir.yusefpasha.taskmanagerapp.data.receiver.AlarmReceiver
 import ir.yusefpasha.taskmanagerapp.domain.service.AlarmService
 import ir.yusefpasha.taskmanagerapp.domain.utils.Constants
@@ -46,11 +47,26 @@ class AlarmServiceImpl(
             PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
 
-        alarmManager.setAndAllowWhileIdle(
-            AlarmManager.RTC_WAKEUP,
-            timestamp,
-            pendingIntent
-        )
+        when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+                if (alarmManager.canScheduleExactAlarms()) {
+                    alarmManager.setExactAndAllowWhileIdle(
+                        AlarmManager.RTC_WAKEUP,
+                        timestamp,
+                        pendingIntent
+                    )
+                }
+            }
+
+            else -> {
+                alarmManager.setAndAllowWhileIdle(
+                    AlarmManager.RTC_WAKEUP,
+                    timestamp,
+                    pendingIntent
+                )
+            }
+        }
+
     }
 
 }
